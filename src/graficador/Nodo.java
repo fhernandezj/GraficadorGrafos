9c104207e52e971f3724f181052cd486b6710e54
unknown <franciscohernandez.22@hotmail.com>
2017-09-07 12:03:59 -0500

    Initial commit

---
+++ src/graficador/Nodo.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graficador;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author FranciscoHdez
+ */
+public class Nodo {
+    int name;
+    int posx, posy;
+    Color color;
+
+    public Nodo(int name, int posx, int posy, Color color) {
+        this.name = name;
+        this.posx = posx;
+        this.posy = posy;
+        this.color = color;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public int getPosx() {
+        return posx;
+    }
+
+    public void setPosx(int posx) {
+        this.posx = posx;
+    }
+
+    public int getPosy() {
+        return posy;
+    }
+
+    public void setPosy(int posy) {
+        this.posy = posy;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+}
+
